52a97867091bf1f2da9d1c44a0d840c395ae78af
Mike Esposito <mikeesposito6852@gmail.com>
2023-06-21 21:46:58 -0700

    Add verify method

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,10 @@
 class RootController < ApplicationController
   def index
   end
+
+  def verify
+    render :json => {
+                      result: Solution.verify_guess(params[:name], params[:xcoord].to_i, params[:ycoord].to_i)
+                    }
+  end
 end
