5cc08f27ec2b56d4eea086937c1b39fa84fd07b5
Mike Esposito <mikeesposito6852@gmail.com>
2023-06-21 21:45:19 -0700

    Add verify_guess method

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,2 +1,8 @@
 class Solution < ApplicationRecord
+  BOUNDARY = 25
+
+  def self.verify_guess(name, xcoord, ycoord)
+    solution = Solution.find_by_name(name)
+    (xcoord - solution.xcoord).abs <= BOUNDARY && (ycoord - solution.ycoord).abs <= BOUNDARY
+  end
 end
